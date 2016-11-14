fbb8040ee041dbc307b1fb551db68409ba5f002e
danny <asdc.sinica@gmail.com>
2016-11-14 09:09:01 +0800

    test

--- run.sh
+++ run.sh
@@ -6,13 +6,4 @@ fi
 
 start_services.sh
 
-if [ ! -f /.db_set ]; then
-	python manage.py packages -o install
-	
-	echo "=> DB Setup Done!"
-	touch /.db_set
-else
-	echo "db already set!"
-fi
-
 python manage.py runserver 0.0.0.0:8080
